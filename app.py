808b6e64b51c01872aaf373677c995d718850ad7
Jugisoft <jukkarasimus81@gmail.com>
2026-02-05 16:00:42 +0200

    app.py

--- app.py
+++ app.py
@@ -3,99 +3,100 @@ import pandas as pd
 from datetime import datetime
 
 # --- ASETUKSET ---
-st.set_page_config(page_title="Pesis-Tilastoija Pro", layout="wide")
+st.set_page_config(page_title="Pesis Live-Syöttö", layout="wide")
 
-# Alustetaan session_state
+# Alustetaan session_state datalle ja valinnoille
 if 'data' not in st.session_state:
     st.session_state.data = pd.DataFrame()
+if 'temp_suunta' not in st.session_state:
+    st.session_state.temp_suunta = ""
 
-# Ladataan listat Excelistä (tässä koodissa kiinteänä Excel-tiedoston pohjalta)
-valinnat = {
-    "Jakso": ["1", "2", "S", "K"],
-    "Tilanne": ["0 tilanne", "1 tilanne", "0-2 tilanne", "0-3 tilanne", "1-2 tilanne", "1-3 tilanne", "2-3 tilanne", "Ajolähtö"],
-    "Lyönti_nro": [1, 2, 3],
-    "Lyönnin_tyyppi": ["Pieni", "Pomppu", "Pussari", "Varsi", "Merkattu kova", "Hämylähtö", "Koppi", "Vapaa", "Kumura"],
-    "Merkattu": ["", "Merkattu"],
-    "Suunta": ["1 raja", "3 luukku", "3 sauma", "3 jatke", "3 taakse", "keskitakanen", "keskisauma", "keskipieni", "2 taakse", "2 sauma", "2 luukku", "2 raja", "keskisauma"],
-    "Tulos": ["palo", "haava", "eteni", "tuottamaton", "onnistunut kentällemeno", "laiton", "vaihto", "takapalo", "takaeteneminen", "juoksu"],
-    "Onnistuminen": ["Onnistunut", "Epäonnistunut"]
-}
+# Listat Excelistä (Syöttölomake & Listat)
+jaksot = ["1", "2", "S", "K"]
+tilanteet = ["0 tilanne", "1 tilanne", "0-2 tilanne", "0-3 tilanne", "1-2 tilanne", "1-3 tilanne", "2-3 tilanne", "Ajolähtö"]
+lyonnit = ["Pieni", "Pomppu", "Pussari", "Varsi", "Merkattu kova", "Hämylähtö", "Koppi", "Vapaa", "Kumura"]
+suunnat = ["1 raja", "3 luukku", "3 sauma", "3 jatke", "3 taakse", "keskitakanen", "keskisauma", "keskipieni", "2 taakse", "2 sauma", "2 luukku", "2 raja"]
+tulokset = ["palo", "haava", "eteni", "tuottamaton", "onnistunut kentällemeno", "laiton", "vaihto", "takapalo", "takaeteneminen", "juoksu"]
 
-st.title("⚾ Pesäpallon Syöttölomake")
+st.title("⚡ Pesis Live-Syöttö")
 
-# --- 1. PERUSTIEDOT (Yläpalkki) ---
-with st.container():
+# --- RIVI 1: PELIN PERUSTIEDOT ---
+with st.expander("Ottelun perustiedot (Aseta kerran)", expanded=False):
     c1, c2, c3, c4 = st.columns(4)
-    pvm = c1.date_input("Päivämäärä", datetime.now())
-    vastustaja = c2.text_input("Vastustaja", "Hyvinkään Tahko")
-    sarja = c3.text_input("Sarja", "MSU")
-    up_kuvio = c4.text_input("UP-KUVIO", "MIKE")
+    vastustaja = c1.text_input("Vastustaja", "Hyvinkään Tahko")
+    sarja = c2.text_input("Sarja", "MSU")
+    up_kuvio = c3.selectbox("UP-KUVIO", ["MIKE", "PERTSA", "PYP"])
+    pvm = c4.date_input("Päivämäärä", datetime.now())
+
+# --- RIVI 2: TILANNE (NAPIT) ---
+st.subheader("1. Tilanne")
+c_j, c_v, c_t, c_p = st.columns([1, 1, 4, 2])
+jakso = c_j.radio("Jakso", jaksot, horizontal=True)
+vuoro = c_v.text_input("Vuoro", "1L")
+tilanne = c_t.select_slider("Pelitilanne", options=tilanteet)
+palot = c_p.radio("Palot ennen", ["0", "1", "2"], horizontal=True)
 
 st.divider()
 
-# --- 2. TILANNE JA LYÖJÄ ---
-col1, col2, col3 = st.columns([1, 1.5, 1.5])
+# --- RIVI 3: LYÖJÄ JA LYÖNNIN LAATU ---
+col_lyoja, col_laatu = st.columns([1, 2])
 
-with col1:
-    st.subheader("Peli")
-    jakso = st.radio("Jakso", valinnat["Jakso"], horizontal=True)
-    vuoropari = st.text_input("Vuoropari (esim. 2L tai 4A)", "1L")
-    tilanne = st.selectbox("Tilanne", valinnat["Tilanne"])
-    palot = st.radio("Palot ennen", ["0", "1", "2"], horizontal=True)
-
-with col2:
-    st.subheader("Lyöjä & Etenijät")
-    lyoja = st.text_input("Lyöjä", "Patrik Wahlsten")
-    lyonti_nro = st.radio("Lyönti nro", valinnat["Lyönti_nro"], horizontal=True)
-    etenija = st.text_input("Etenijä", "")
-    t_etenija1 = st.text_input("Takaetenijä 1", "")
-    t_etenija2 = st.text_input("Takaetenijä 2", "")
-
-with col3:
-    st.subheader("Lyönnin laatu")
-    l_tyyppi = st.selectbox("Lyönnin tyyppi", valinnat["Lyönnin_tyyppi"])
-    merkattu = st.radio("Merkattu", valinnat["Merkattu"], horizontal=True)
-    sijainti = st.selectbox("Lyönnin sijainti (Suunta)", valinnat["Suunta"])
-    lukkari_toiminto = st.text_input("Lukkarin toiminto", "")
-
-st.divider()
-
-# --- 3. TULOS ---
-col_t1, col_t2 = st.columns(2)
-
-with col_t1:
-    st.subheader("Lopputulos")
-    l_tulos = st.radio("Lyönnin tulos", valinnat["Tulos"], horizontal=True)
-    onnistuminen = "Onnistunut" if l_tulos in ["juoksu", "vaihto", "eteni", "onnistunut kentällemeno"] else "Epäonnistunut"
-    st.info(f"Päätelty onnistuminen: **{onnistuminen}**")
-
-with col_t2:
-    st.subheader("Lisätiedot")
-    o_tarkenne = st.text_input("Onnistumisen tarkenne", "")
-    toiminto_lyoja = st.text_input("Toiminto lyöjällä", "")
-    suorittava_up = st.text_input("Suorittava ulkopelaaja", "")
-
-# --- TALLENNUS ---
-if st.button("TALLENNA RIVI EXCELIIN", type="primary", use_container_width=True):
-    uusi_rivi = {
-        "Päivämäärä": pvm, "Vastustaja": vastustaja, "Jakso": jakso, "Vuoropari": vuoropari,
-        "Tilanne": tilanne, "Lyöjä": lyoja, "Lyönti nro": lyonti_nro, "Etenijä": etenija,
-        "Palot ennen lyöntiä": palot, "Lyönnin tyyppi": l_tyyppi, "Merkattu": merkattu,
-        "Lyönnin sijainti": sijainti, "Lyönnin tulos": l_tulos, "Onnistuminen": onnistuminen,
-        "Onnistumisen tarkenne": o_tarkenne, "Toiminto lyöjällä": toiminto_lyoja,
-        "Suorittava ulkopelaaja": suorittava_up, "Lukkarin toiminto": lukkari_toiminto,
-        "Sarja": sarja, "Takaetenijä 1": t_etenija1, "Takaetenijä 2": t_etenija2, "UP-KUVIO": up_kuvio
-    }
+with col_lyoja:
+    st.subheader("2. Lyöjä")
+    lyoja_nimi = st.text_input("Lyöjän nimi", "Patrik Wahlsten")
+    lyonti_nro = st.radio("Lyönti nro", [1, 2, 3], horizontal=True)
+    merkattu = st.toggle("MERKKI PÄÄLLÄ")
     
-    st.session_state.data = pd.concat([pd.DataFrame([uusi_rivi]), st.session_state.data], ignore_index=True)
-    st.success("Tapahtuma tallennettu listaan!")
+with col_laatu:
+    st.subheader("3. Lyönnin tyyppi")
+    # Tehdään tyypeistä isoja nappeja
+    l_tyyppi = st.radio("Valitse tyyppi", lyonnit, horizontal=True)
+    
+    st.subheader("4. Suunta")
+    # Suunnat sarakkeittain (3-puoli, Keskus, 2-puoli)
+    s1, s2, s3 = st.columns(3)
+    with s1:
+        if st.button("3 Jatke", use_container_width=True): st.session_state.temp_suunta = "3 jatke"
+        if st.button("3 Luukku", use_container_width=True): st.session_state.temp_suunta = "3 luukku"
+        if st.button("3 Sauma", use_container_width=True): st.session_state.temp_suunta = "3 sauma"
+        if st.button("3 Taakse", use_container_width=True): st.session_state.temp_suunta = "3 taakse"
+    with s2:
+        if st.button("Keskitaakse", use_container_width=True): st.session_state.temp_suunta = "keskitakanen"
+        if st.button("Keskisauma", use_container_width=True): st.session_state.temp_suunta = "keskisauma"
+        if st.button("Keskipieni", use_container_width=True): st.session_state.temp_suunta = "keskipieni"
+    with s3:
+        if st.button("2 Jatke", use_container_width=True): st.session_state.temp_suunta = "2 jatke"
+        if st.button("2 Luukku", use_container_width=True): st.session_state.temp_suunta = "2 luukku"
+        if st.button("2 Sauma", use_container_width=True): st.session_state.temp_suunta = "2 sauma"
+        if st.button("1 Raja", use_container_width=True): st.session_state.temp_suunta = "1 raja"
+    
+    st.info(f"Valittu suunta: **{st.session_state.temp_suunta}**")
 
-# --- NÄKYMÄ ---
 st.divider()
-st.subheader("Tallennetut rivit")
-st.dataframe(st.session_state.data, use_container_width=True)
 
-# Lataus
-if not st.session_state.data.empty:
-    csv = st.session_state.data.to_csv(index=False).encode('utf-8-sig')
-    st.download_button("📥 Lataa CSV (Excel-yhteensopiva)", data=csv, file_name="pesis_tilastot.csv", mime="text/csv")
+# --- RIVI 4: TULOS JA TALLENNUS ---
+st.subheader("5. Tulos")
+c_tulos, c_lisa = st.columns([2, 1])
+
+with c_tulos:
+    l_tulos = st.radio("Lyönnin tulos", tulokset, horizontal=True)
+    onnistuminen = "Onnistunut" if l_tulos in ["juoksu", "vaihto", "eteni", "onnistunut kentällemeno"] else "Epäonnistunut"
+
+with c_lisa:
+    etenija = st.text_input("Etenijä", "")
+    up_suorittaja = st.text_input("UP-suorittaja", "")
+
+if st.button("💾 TALLENNA TAPAHTUMA", type="primary", use_container_width=True):
+    uusi_rivi = {
+        "Päivämäärä": pvm, "Vastustaja": vastustaja, "Jakso": jakso, "Vuoropari": vuoro,
+        "Tilanne": tilanne, "Lyöjä": lyoja_nimi, "Lyönti nro": lyonti_nro, "Etenijä": etenija,
+        "Palot ennen lyöntiä": palot, "Lyönnin tyyppi": l_tyyppi, "Merkattu": "Merkattu" if merkattu else "",
+        "Lyönnin sijainti": st.session_state.temp_suunta, "Lyönnin tulos": l_tulos, "Onnistuminen": onnistuminen,
+        "Suorittava ulkopelaaja": up_suorittaja, "Sarja": sarja, "UP-KUVIO": up_kuvio
+    }
+    st.session_state.data = pd.concat([pd.DataFrame([uusi_rivi]), st.session_state.data], ignore_index=True)
+    st.session_state.temp_suunta = "" # Nollaus
+    st.success("Tallennettu!")
+
+# --- DATAN TARKASTELU ---
+st.dataframe(st.session_state.data.head(5), use_container_width=True)
